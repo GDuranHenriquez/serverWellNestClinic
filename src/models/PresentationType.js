98cd6c6ec6cafffc105e4f7d5d0337d6a5578a56
Luismiferal <Luismiferal@gmail.com>
2023-08-29 15:10:47 -0400

    agregando los models restantes. LuisF

---
+++ src/models/PresentationType.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require('sequelize/types');
+
+module.exports = (sequelize) => {
+  
+  sequelize.define('presentationtype', {
+    id:{
+      type : DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,      
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+},
+{
+     timestamps: false,
+     createAt: false,
+     updateAt: false
+ })}
