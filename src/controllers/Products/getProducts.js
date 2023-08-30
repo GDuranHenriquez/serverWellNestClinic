aa1c9618834f1862760a4d036a094e80744722b8
manuel agradi <agradimanuel@gmail.com>
2023-08-29 23:18:20 -0300

    Product: modificando el modelo product

--- src/controllers/Products/getProducts.js
+++ src/controllers/Products/getProducts.js
@@ -2,7 +2,7 @@ const { Product } = require("../../db")
 
 const getProducts = async (req, res) => {
     try {
-        const products = await Product.findAll({where: {delete: false}});
+        const products = await Product.findAll({where: {deleted: false}});
         return res.status(200).json(products);
     } catch (error) {
         return res.status(500).json({error: error.message});
