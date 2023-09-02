4448cad30c349a9a2fba1c0560d3fcfb2b9c9c97
GDHenriquez <gregorioduranedrian123@gmail.com>
2023-09-02 17:19:57 -0400

    Fix: Fixing  New erros GDuran

--- src/routes/routerSale.js
+++ src/routes/routerSale.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 
-const { getSaleByUser } = require("../controllers/sale/getSale.js");
+const { getSaleByUser } = require("../controllers/sale/getSale");
 const routerSaleByUser = Router();
 routerSaleByUser.get("/:userId", getSaleByUser);
 
