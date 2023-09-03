ab811e67a61b819f13a1f6a70bcde49d7d34d69b
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2023-09-03 00:17:01 -0300

    GetSpeciality fixed. Fran

--- src/controllers/Speciality/getSpecialities.js
+++ src/controllers/Speciality/getSpecialities.js
@@ -2,7 +2,10 @@ const { Speciality } = require("../../db");
 
 const getSpecialities = async (req, res) => {
     try {
-        const specialities = Speciality.findAll();
+        const specialities = await Speciality.findAll();
+        if(!Object.keys(specialities).length) {
+            return res.status(404).json({error: 'There is no Speciality loaded yet'})
+        }
         return res.status(200).json(specialities);
     } catch (error) {
         res.status(500).json({error: error.message});
