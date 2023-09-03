07f7271b0531f2dc759d666255835bd1c86c3a3c
manuel agradi <agradimanuel@gmail.com>
2023-09-03 19:49:26 -0300

    Arreglando post doctor

--- src/controllers/doctor/postDoctor.js
+++ src/controllers/doctor/postDoctor.js
@@ -27,23 +27,3 @@ try {
 };
 
 module.exports = { postDoctor };
-
-// const postActivity = async (req, res) => {
-//     const { name, difficulty, season, countries } = req.body
-//     try {
-//         if (!name || !difficulty || !season || !countries) throw Error("Faltan datos")
-//         const respuesta = await Activity.create({ name, difficulty, season });
-//         await respuesta.setCountries(countries);
-//         const actividadCreada = await Activity.findOne({
-//             where: { id: respuesta.id }, include: {
-//                 model: Country, through: {
-//                     attribute: [],
-//                 },
-//             }
-//         });
-
-//         return res.status(200).json({ message: "Actividad creada correctamente", actividadCreada });
-//     } catch (error) {
-//         return res.status(404).send({ error: error.message });
-//     }
-// }
