914a29f88311856136f04a8ea99b21d75e87e3e6
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2023-09-04 11:55:16 -0300

    fix doctorPost. Fran

--- src/controllers/doctor/postDoctor.js
+++ src/controllers/doctor/postDoctor.js
@@ -3,11 +3,11 @@ const { Op } = require("sequelize");
 
 const postDoctor = async (req, res) => {
 try {
-    const { name, lastName, phone, email, speciality, address } = req.body;
-    if(!name || !lastName || !phone || !email || !address || !speciality.length) {
+    const { name, lastName, phone, email, specialities, address } = req.body;
+    if(!name || !lastName || !phone || !email || !address || !specialities.length) {
         return res.status(403).json({error: "Mandatory data is missing"});
     }
-    const specialitiesIds = speciality.map((sp) => parseInt(sp))
+    const specialitiesIds = specialities.map((sp) => parseInt(sp))
     const findedSpecialities = await Speciality.findAll({where: {id: {[Op.in]: specialitiesIds}}})
 
     if(Object.keys(findedSpecialities).length === 0) {
@@ -24,11 +24,11 @@ try {
             phone: doctor.phone,
             email: doctor.email,
             address: doctor.address,
-            speciality: findedSpecialities
+            specialities: findedSpecialities
         }
         return res.status(200).json(response);
     }
-    return res.status(403).json({error: `Doctor's Email: ${email}, allready exist in data base`})
+    return res.status(403).json({error: `Doctor's Email: "${email}", allready exist in data base`})
     
 } catch (error) {
     return res.status(500).send({error: error.message});
