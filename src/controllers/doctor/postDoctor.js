53aa384dd1a171f613024865bb5b7e583097930b
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2023-09-04 11:52:10 -0300

    fix doctorPost. Fran

--- src/controllers/doctor/postDoctor.js
+++ src/controllers/doctor/postDoctor.js
@@ -1,29 +1,38 @@
 const { Doctor, Speciality } = require("../../db");
 const { Op } = require("sequelize");
-const { all } = require("../../routes/routerDoctor");
 
 const postDoctor = async (req, res) => {
 try {
     const { name, lastName, phone, email, speciality, address } = req.body;
-    if(!name || !lastName || !phone || !email || !address || !speciality) {
+    if(!name || !lastName || !phone || !email || !address || !speciality.length) {
         return res.status(403).json({error: "Mandatory data is missing"});
     }
-    const findSpeciality = speciality.map((sp) => parseInt(sp))
-    const especialidad = await Speciality.findAll({where: {id: {[Op.in]: findSpeciality}}})
+    const specialitiesIds = speciality.map((sp) => parseInt(sp))
+    const findedSpecialities = await Speciality.findAll({where: {id: {[Op.in]: specialitiesIds}}})
 
-    if(Object.keys(especialidad).length === 0) {
-        return res.status(403).json({error: "No existen las especialidades requeridas"})
+    if(Object.keys(findedSpecialities).length === 0) {
+        return res.status(403).json({error: "The required specialities do not exist"})
     }
-
-    console.log((especialidad))
     
-    const doctor = await Doctor.create({name, lastName, email, phone, address});
-          doctor.addSpeciality(especialidad);
-
-    return res.status(200).json({...doctor, especialidad});
-} catch (error) {
-    return res.status(404).send({error: error.message});
+    const [doctor, created] = await Doctor.findOrCreate({where:{email}, defaults: {name, lastName,phone, address }});
+    if(created){
+        doctor.addSpeciality(findedSpecialities);
+        const response = {
+            id: doctor.id,
+            name: doctor.name,
+            lastName: doctor.lastName,
+            phone: doctor.phone,
+            email: doctor.email,
+            address: doctor.address,
+            speciality: findedSpecialities
+        }
+        return res.status(200).json(response);
     }
+    return res.status(403).json({error: `Doctor's Email: ${email}, allready exist in data base`})
+    
+} catch (error) {
+    return res.status(500).send({error: error.message});
+}
 };
 
 module.exports = { postDoctor };
