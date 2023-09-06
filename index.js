e8d7aba36dec8ca07381a749ab2ef14cdf8a9730
GDHenriquez <gregorioduranedrian123@gmail.com>
2023-09-06 12:33:25 -0400

    No trunc

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { PORT_LISTENER, FORCE_DB } = process.env;
 
 
 // Syncing all the models at once.
-conn.sync({ force: FORCE_DB }).then(() => {
+conn.sync({ force: false }).then(() => {
       server.listen(PORT_LISTENER, () => {
       console.log('%s listening at 3001'); // eslint-disable-line no-console
     });  
