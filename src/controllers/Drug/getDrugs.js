e5880e8bf3b880564299824cb386575c58babed9
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2023-09-11 15:07:00 -0300

    fix messages. Fran

---
+++ src/controllers/Drug/getDrugs.js
@@ -0,0 +1,10 @@
+const { Drugs } = require("../../db")
+const getDrugs = async (req, res) => {
+    try {
+        const drugs = await Drugs.findAll();
+        return res.status(200).json(drugs);
+    } catch (error) {
+        return res.status(500).json({error: error.message});
+    }
+}
+module.exports = getDrugs;
