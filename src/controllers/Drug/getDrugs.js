cc1eb13c442994706d5ac640462bb03f46a23006
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2023-09-12 21:47:04 -0300

    Fix export get drug. Fran

--- src/controllers/Drug/getDrugs.js
+++ src/controllers/Drug/getDrugs.js
@@ -1,7 +1,7 @@
-const { Drugs } = require("../../db")
+const { Drug } = require("../../db")
 const getDrugs = async (req, res) => {
     try {
-        const drugs = await Drugs.findAll();
+        const drugs = await Drug.findAll();
         return res.status(200).json(drugs);
     } catch (error) {
         return res.status(500).json({error: error.message});
