a6ceabe4ef8f7b5592fce8a1ff969da5ad421802
GDHenriquez <gregorioduranedrian123@gmail.com>
2023-09-14 02:00:06 -0400

    feature/modelsControlers: appointment bug fixin post, get

--- src/controllers/appointment/deleteAppointment.js
+++ src/controllers/appointment/deleteAppointment.js
@@ -4,7 +4,11 @@ const deleteAppointment = async (req, res) => {
     try {
         const {appointmentId} = req.params
         const appointment = await Appointment.findOne({where: {id: appointmentId}, include: [{model: StatusAppointment, as: "Status_Appointment"}]})
-        if(appointment.Status_Appointment.status !== 'cancel'){
+        
+        if(appointment.Status_Appointment.status === 'close'){
+            return res.status(404).json({error: 'Appointment cannot be cancelled, this appointment has already been closed.'})
+        }
+        if(appointment.Status_Appointment.status === 'open'){
             const status = await StatusAppointment.findOne({where: {status: 'cancel'}})
             if(!status) {
                 return res.status(404).json({error: 'Cancel status not registered'})
@@ -12,8 +16,7 @@ const deleteAppointment = async (req, res) => {
             appointment.setStatus_Appointment(status.id);
             return res.status(200).json({message: 'Appointment successfully  canceled'})
         }
-        return res.status(403).json({error: 'Appointment already canceled'})
-        
+        return res.status(403).json({error: 'Appointment already canceled'})        
 
     } catch (error) {
         return res.status(500).json({error: error.message})
