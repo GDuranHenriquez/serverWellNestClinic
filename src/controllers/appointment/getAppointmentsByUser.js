7c6065ef3f23af6412f1638b21c898eaa207e158
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2023-09-14 11:56:08 -0300

    improved getAppointmentByUser. Fran

--- src/controllers/appointment/getAppointmentsByUser.js
+++ src/controllers/appointment/getAppointmentsByUser.js
@@ -3,58 +3,30 @@ const {Appointment, Doctor, UserClient ,StatusAppointment, Speciality} = require
 const getAppointmentsByUser = async (req, res) => {
     try {
         const {userId, statusId} = req.query;
+        const where = {}
+        if(userId){
+            where.userClient = userId
+        }
+        if(statusId){
+            where.StatusAppointmentId = statusId
+        }
 
-        if(userId && statusId){
-            const appointments = await Appointment.findAll({
-                where: {userClient: userId, StatusAppointmentId: statusId},
-                attributes: ['id', 'date', 'startTime'],
-                include: [
-                    {model: Doctor, as: "Appointment_Doctor", 
-                    attributes: ['id', 'name', 'lastName']},
-                    {model: StatusAppointment, as: 'Status_Appointment'},
-                    {model: UserClient, as: 'Appointment_UserClient',
-                    attributes: ['id', 'name', 'lastName', 'email', 'backupContact', 'imageUrl']},
-                    {model: Speciality, as: 'Appointment_Speciality'}]
-            });
-            if(!appointments.length) {
-                return res.status(200).json({error: "You don't have any appointment scheduled yet"})
-            } else {
-                return res.status(200).json(appointments)
-            }
-        }else if(userId){
-            const appointments = await Appointment.findAll({
-                where: {userClient: userId},
-                attributes: ['id', 'date', 'startTime'],
-                include: [
-                    {model: Doctor, as: "Appointment_Doctor", 
-                    attributes: ['id', 'name', 'lastName']},
-                    {model: StatusAppointment, as: 'Status_Appointment'},
-                    {model: UserClient, as: 'Appointment_UserClient',
-                    attributes: ['id', 'name', 'lastName', 'email', 'backupContact', 'imageUrl']},
-                    {model: Speciality, as: 'Appointment_Speciality'}]
-            });
-            if(!appointments.length) {
-                return res.status(200).json({error: "You don't have any appointment scheduled yet"})
-            } else {
-                return res.status(200).json(appointments)
-            }
-        }else{
-            const appointments = await Appointment.findAll({
-                attributes: ['id', 'date', 'startTime'],
-                include: [
-                    {model: Doctor, as: "Appointment_Doctor", 
-                    attributes: ['id', 'name', 'lastName']},
-                    {model: StatusAppointment, as: 'Status_Appointment'},
-                    {model: UserClient, as: 'Appointment_UserClient',
-                    attributes: ['id', 'name', 'lastName', 'email', 'backupContact', 'imageUrl']},
-                    {model: Speciality, as: 'Appointment_Speciality'}]
-            });
-            if(!appointments.length) {
-                return res.status(200).json({error: "You don't have any appointment scheduled yet"})
-            } else {
-                return res.status(200).json(appointments)
-            }
-        };
+        const appointments = await Appointment.findAll({
+            where,
+            attributes: ['id', 'date', 'startTime'],
+            include: [
+                {model: Doctor, as: "Appointment_Doctor", 
+                attributes: ['id', 'name', 'lastName']},
+                {model: StatusAppointment, as: 'Status_Appointment'},
+                {model: UserClient, as: 'Appointment_UserClient',
+                attributes: ['id', 'name', 'lastName', 'email', 'backupContact', 'imageUrl']},
+                {model: Speciality, as: 'Appointment_Speciality'}]
+        });
+        if(!appointments.length) {
+            return res.status(200).json({error: "You don't have any appointment scheduled yet" + (statusId ? " with the specified status" : "")})
+        } else {
+            return res.status(200).json(appointments)
+        }
     } catch (error) {
         return res.status(500).json({error: error.message})
     }
