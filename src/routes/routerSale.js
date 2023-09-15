3d29d0a25a674912aa49b8f44c4feff264243b8b
GDHenriquez <gregorioduranedrian123@gmail.com>
2023-09-15 13:53:41 -0400

    Merge branch with feature/models

--- src/routes/routerSale.js
+++ src/routes/routerSale.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 
 const { getSaleByUser } = require('../controllers/Sale/getSale');
-const {postSale} = require('../controllers/sale/postSale')
+const {postSale} = require('../controllers/Sale/postSale');
 
 const routerSaleByUser = Router();
 routerSaleByUser.get("/:user", getSaleByUser);
