e1dc5d054d7e04d58c7852d10491e69f109e82e3
Francisco Hernandez Valenzuela <franciscohv95@gmail.com>
2023-09-15 03:49:36 -0300

    postSale improvements. Fran

--- src/controllers/Sale/postSale.js
+++ src/controllers/Sale/postSale.js
@@ -1,13 +1,13 @@
 const Stripe = require("stripe");
-const {Sale, Cart, Product, DetailSale} = require('../../db');
+const {Sale, Cart, Product, DetailSale, UserClient, Plan} = require('../../db');
 require('dotenv').config();
 const {SKSTRIPE} = process.env
 const stripe = new Stripe(SKSTRIPE);
 
 const postSale = async (req, res) => {
     try {
-        const {stripeId, price, cartId, user, discount} = req.body;
-        if(!stripeId || !price || !cartId || !user) {
+        const {stripeId, cartId, user} = req.body;
+        if(!stripeId || !cartId || !user) {
             return res.status(403).json({error: 'Mandatory data is missing'})
         }
         const cart = await Cart.findOne({where: {id: cartId}, include: [{model: Product}]})
@@ -17,11 +17,24 @@ const postSale = async (req, res) => {
         if(cart.user !== user) {
             return res.status(401).json({error: "The user doesn't seem to be the owner of this cart"})
         }
+        const userClient = await UserClient.findOne({where: {id: user}, attributes: [], include: [{model: Plan, as: "UserClient_Plan"}]})
+        let discount = 0.5 // ==> se obtiene del plan
         const date = new Date()
-        const saleData = {stripeId, price, date, user}
+        const saleData = {stripeId, date, user}
+        let priceTot = 0
+        for(const product of cart.products){
+            priceTot += product.price
+        }
         if(discount){
             saleData.discount = discount
         }
+        saleData.price = discount ? (priceTot * (1 - discount)) : priceTot
+        // const payment = await stripe.paymentIntents.create({
+            //     priceTot,
+            //     payment_method: stripeId,
+            //     confirm: true
+            // })
+
         const sale = await Sale.create(saleData)
         const detailData = cart.products.map((product) => ({
             amount: product.cart_product.amount,
@@ -30,11 +43,6 @@ const postSale = async (req, res) => {
             product: product.id
         }))
         const detailSale = await DetailSale.bulkCreate(detailData)
-        // const PayM = await stripe.paymentIntents.create({
-            //     amount,
-            //     payment_method: id,
-            //     confirm: true
-            // })
         await cart.destroy()
 
         // Logica para manejar el envio de correo al usuario con la info de la compra
