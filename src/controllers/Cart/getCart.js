a6ebc7c2e8f4409bf2c03900000a5b37e6e23bd0
GDHenriquez <gregorioduranedrian123@gmail.com>
2023-09-16 03:19:43 -0400

    develop: Cart and nodemailer GDuran

--- src/controllers/Cart/getCart.js
+++ src/controllers/Cart/getCart.js
@@ -6,6 +6,16 @@ const getCart = async (req, res) => {
         if(!user){
             return res.status(403).json({error: "Mandatory data is missing"})
         }
+        const userPlan = await UserClient.findByPk(user, {
+            include: [{
+                model: Plan,
+                as: "UserClient_Plan"
+            }],
+        });
+        if(!userPlan) {
+            return res.status(404).json({error: "User plan not found"});
+        }
+
         const cart = await Cart.findOne({where: {user},
             attributes:['id'],
             include: [
@@ -21,7 +31,16 @@ const getCart = async (req, res) => {
         if(!cart){
             return res.status(404).json({error: "Cart not found"})
         }
-        return res.status(200).json(cart)
+
+        for(const product of cart.products) {
+            totalPrice += (product.price * product.cart_product.amount)
+        }
+
+        const discount = userPlan.UserClient_Plan.discount;
+        const discountedPrice = calculateDiscountedPrice(totalPrice, discount);
+        cart.discountedPrice = discountedPrice;
+
+        return res.status(200).json({cart, discountedPrice})
     } catch (error) {
         return res.status(500).json({error: error.message})
     }
