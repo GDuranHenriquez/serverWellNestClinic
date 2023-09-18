8fb620e8226498af62a5df64f2d9b2e1d8c5a78f
GDHenriquez <gregorioduranedrian123@gmail.com>
2023-09-18 19:00:20 -0400

    fix get product GDuran

--- src/controllers/Cart/postCart.js
+++ src/controllers/Cart/postCart.js
@@ -6,49 +6,53 @@ const postCart = async (req, res) => {
     if ((!user, !productId, !Number(amount) < 0)) {
       return res.status(403).json({ error: "Mandatory data is missing" });
     }
+
+    const response = await Cart.findOrCreate({ where: { user } });
+    const cartInstance = response[0];
+
     if (Number(amount) === 0) {
+      await cartInstance.removeProduct(productId);
     } else {
-      const response = await Cart.findOrCreate({ where: { user } });
       await response[0].addProduct(productId, { through: { amount } });
-
-      const userPlan = await UserClient.findByPk(user, {
-        include: [
-          {
-            model: Plan,
-            as: "UserClient_Plan",
-          },
-        ],
-      });
-
-      if (!userPlan) {
-        return res.status(404).json({ error: "User plan not found" });
-      }
-
-      const cart = await Cart.findOne({
-        where: { id: response[0].id },
-        attributes: ["id"],
-        include: [
-          {
-            model: Product,
-            attributes: ["name", "price"],
-            through: {
-              attributes: ["amount"],
-            },
-          },
-        ],
-      });
-
-      let totalPrice = 0;
-
-      for (const product of cart.products) {
-        totalPrice += product.price * product.cart_product.amount;
-      }
-
-      const discount = userPlan.UserClient_Plan.discount;
-      const discountedPrice = calculateDiscountedPrice(totalPrice, discount);
-      cart.discountedPrice = discountedPrice;
-      return res.status(200).json({ cart, discountedPrice });
     }
+
+    const userPlan = await UserClient.findByPk(user, {
+      include: [
+        {
+          model: Plan,
+          as: "UserClient_Plan",
+        },
+      ],
+    });
+
+    if (!userPlan) {
+      return res.status(404).json({ error: "User plan not found" });
+    }
+
+    const cart = await Cart.findOne({
+      where: { id: response[0].id },
+      attributes: ["id"],
+      include: [
+        {
+          model: Product,
+          attributes: ["name", "price"],
+          through: {
+            attributes: ["amount"],
+          },
+        },
+      ],
+    });
+
+    let totalPrice = 0;
+
+    for (const product of cart.products) {
+      totalPrice += product.price * product.cart_product.amount;
+    }
+
+    const discount = userPlan.UserClient_Plan.discount;
+    const discountedPrice = calculateDiscountedPrice(totalPrice, discount);
+    cart.discountedPrice = discountedPrice;
+    return res.status(200).json({ cart, discountedPrice });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
