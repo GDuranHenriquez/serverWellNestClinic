86ac442bc30e2f8176c7d6c3cc9704df872163c2
GDHenriquez <gregorioduranedrian123@gmail.com>
2023-09-18 13:40:13 -0400

    get cart GDuran

--- src/controllers/Cart/getCart.js
+++ src/controllers/Cart/getCart.js
@@ -1,30 +1,58 @@
-const {Cart, Product} = require('../../db')
+const {Cart, Product, UserClient, Plan} = require('../../db')
 
 const getCart = async (req, res) => {
     try {
         const {user} = req.params
+
         if(!user){
             return res.status(403).json({error: "Mandatory data is missing"})
         }
+
+        const userPlan = await UserClient.findByPk(user, {
+            include: [{
+                model: Plan,
+                as: "UserClient_Plan"
+            }],
+        });
+        if(!userPlan) {
+            return res.status(404).json({error: "User plan not found"});
+        }
+
         const cart = await Cart.findOne({where: {user},
             attributes:['id'],
             include: [
                 {
                     model: Product,
-                    attributes: ['name', 'price'],
+                    attributes: ['id','name', 'price'],
                     through: {
                         attributes: ['amount'],
                     },
                 },
             ]
         })
+
         if(!cart){
             return res.status(404).json({error: "Cart not found"})
         }
-        return res.status(200).json(cart)
+
+        let totalPrice = 0;
+        for(const product of cart.products) {
+            totalPrice += (product.price * product.cart_product.amount)
+        }
+
+        const discount = userPlan.UserClient_Plan.discount;
+        const discountedPrice = calculateDiscountedPrice(totalPrice, discount);
+        cart.discountedPrice = discountedPrice;
+
+        return res.status(200).json({cart, discountedPrice})
     } catch (error) {
         return res.status(500).json({error: error.message})
     }
+}
+
+function calculateDiscountedPrice(price, discount) {
+    const discountedPrice = price * (1- discount);
+    return discountedPrice;
 }
 
 module.exports = getCart
